fa716b8c46725197fee96ea40ffccc625112c3df
Administrator <Administrator@CT2N190M5AL8798>
2019-06-14 11:10:30 +0800

    niuwajun

--- mall-admin/src/main/java/com/macro/mall/config/SecurityConfig.java
+++ mall-admin/src/main/java/com/macro/mall/config/SecurityConfig.java
@@ -37,7 +37,9 @@ import java.util.List;
  */
 @Configuration
 @EnableWebSecurity
+//通过 @EnableWebSecurity注解开启Spring Security的功能
 @EnableGlobalMethodSecurity(prePostEnabled = true)
+//@EnableGlobalMethodSecurity(prePostEnabled = true)这个注解，可以开启security的注解，我们可以在需要控制权限的方法上面使用@PreAuthorize，@PreFilter这些注解。
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private UmsAdminService adminService;
@@ -48,10 +50,41 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.csrf()// 由于使用的是JWT，我们这里不需要csrf
-                .disable()
+        httpSecurity.csrf().disable()// crsf禁用,由于使用的是JWT，不是session,我们这里不需要csrf
                 .sessionManagement()// 基于token，所以不需要session
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                
+//                .authorizeRequests()
+//                .antMatchers("/image/**").permitAll() //静态资源访问无需认证
+//                .antMatchers("/admin/**").hasAnyRole("ADMIN") //admin开头的请求，需要admin权限
+//                .antMatchers("/article/**").hasRole("USER") //需登陆才能访问的url
+//                .anyRequest().authenticated()  //默认其它的请求都需要认证，这里一定要添加
+//                .and()
+//            .csrf().disable()  //CRSF禁用，因为不使用session
+//            .sessionManagement().disable()  //禁用session
+//            .formLogin().disable() //禁用form登录
+//            .cors()  //支持跨域
+//            .and()   //添加header设置，支持跨域和ajax请求
+//            .headers().addHeaderWriter(new StaticHeadersWriter(Arrays.asList(
+//                    new Header("Access-control-Allow-Origin","*"),
+//                    new Header("Access-Control-Expose-Headers","Authorization"))))
+//            .and() //拦截OPTIONS请求，直接返回header
+//            .addFilterAfter(new OptionRequestFilter(), CorsFilter.class)
+//            //添加登录filter
+//            .apply(new JsonLoginConfigurer<>()).loginSuccessHandler(jsonLoginSuccessHandler())
+//            .and()
+//           //添加token的filter
+//            .apply(new JwtLoginConfigurer<>()).tokenValidSuccessHandler(jwtRefreshSuccessHandler()).permissiveRequestUrls("/logout")
+//            .and()
+//            //使用默认的logoutFilter
+//            .logout()
+////              .logoutUrl("/logout")   //默认就是"/logout"
+//                .addLogoutHandler(tokenClearLogoutHandler())  //logout时清除token
+//                .logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler()) //logout成功后返回200
+//            .and()
+//            .sessionManagement().disable();
+                
+                
                 .and()
                 .authorizeRequests()
                 .antMatchers(HttpMethod.GET, // 允许对于网站静态资源的无授权访问
@@ -63,16 +96,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         "/**/*.js",
                         "/swagger-resources/**",
                         "/v2/api-docs/**"
-                )
-                .permitAll()
+                ).permitAll()
                 .antMatchers("/admin/login", "/admin/register")// 对登录注册要允许匿名访问
                 .permitAll()
                 .antMatchers(HttpMethod.OPTIONS)//跨域请求会先进行一次options请求
                 .permitAll()
                 .antMatchers("/**")//测试时全部运行访问
                 .permitAll()
-                .anyRequest()// 除上面外的所有请求全部需要鉴权认证
-                .authenticated();
+                .anyRequest().authenticated();// 除上面外的所有请求全部需要鉴权认证
         // 禁用缓存
         httpSecurity.headers().cacheControl();
         // 添加JWT filter
